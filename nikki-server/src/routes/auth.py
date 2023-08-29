9fad8120082f7e74166e59351300a4c8d291b776
koguchi212 <akira.kouguchi@gmail.com>
2023-08-29 09:55:00 +0900

    queryを変更して非同期処理のログインに成功しました

--- nikki-server/src/routes/auth.py
+++ nikki-server/src/routes/auth.py
@@ -4,6 +4,7 @@ from .. import models, token
 from src.database import get_db
 from .. hashing import verify_password
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from src.log import get_logger
 
 
@@ -16,7 +17,15 @@ router=APIRouter(
 
 @router.post('/login')
 async def login(request:OAuth2PasswordRequestForm=Depends(), db:AsyncSession=Depends(get_db)):
-    user=db.query(models.User).filter(models.User.email==request.username).first()
+    async with db as session:  # 非同期セッションを直接使用
+        # クエリを作成
+        stmt = select(models.User).filter(models.User.email == request.username)
+
+        # クエリを実行し、結果を取得
+        result = await session.execute(stmt)
+
+        # ユーザーを取得
+        user = result.scalar()
     if not user:
         auth_logger.info(f'emailの認証情報が無効です: {request.username}')
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Invalid Credentials')
